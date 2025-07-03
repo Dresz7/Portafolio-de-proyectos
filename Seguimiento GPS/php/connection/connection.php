d4176fd5347b9de39c2dff2dd04e24bf2645f43a
Dresz7 <138611595+Dresz7@users.noreply.github.com>
2025-07-02 17:44:23 -0700

    Delete Seguimiento GPS directory
    Borrado por petición de la empresa

--- Seguimiento GPS/php/connection/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conexion = new mysqli("localhost", "root", "", "BajaRent");
-$conexion->set_charset("utf8");
-
-mysqli_query($conexion, "SET NAMES 'utf8'");
-
-if ($conexion->connect_errno) {
-    echo "FALLO AL CONECTAR A LA BD: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
-}
